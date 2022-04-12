6d2cef40c57e902d4f74049f8c22554e33426585
jaimechacon11 <chacon.jaime99g@gmail.com>
2022-04-12 00:10:30 -0500

    Ejercicios Semana 2

---
+++ weekly_mission_2/exercises/examples/1.Objetos/examples_2.js
@@ -0,0 +1,9 @@
+console.log("Objetos");
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+  name: "Carlo",
+  age: 27,
+};
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+console.log(myObjetc2);
